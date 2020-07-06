71936d4815f54d71bad4ba0a82e9c4cd0aee8581
Idris Bowman <bowmanidris95@gmail.com>
2020-07-05 20:39:08 -0400

    add query param to app to retain filter

--- src/app/products/product-routing.module.ts
+++ src/app/products/product-routing.module.ts
@@ -5,6 +5,7 @@ import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
+// Add routes with required params
 const ROUTES = [
     { path: 'products', component: ProductListComponent},
     { path: 'products/:id', component: ProductDetailComponent},
